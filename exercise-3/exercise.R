9c65e03e1cfad43381414507220b0ef9d7f08de9
Michael Freeman <michael.k.freeman@gmail.com>
2018-04-04 11:27:12 -0700

    Fix dot notation error and stylize

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -3,22 +3,22 @@
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
 add_three <- function(value) {
-  value + 3  # return the result
+  value + 3 # return the result
 }
 
-# Create a variable `ten` that is the result of passing 7 to your `add_three` 
+# Create a variable `ten` that is the result of passing 7 to your `add_three`
 # function
 ten <- add_three(7)
 
-# Define a function `imperial_to_metric` that takes in two arguments: a number 
+# Define a function `imperial_to_metric` that takes in two arguments: a number
 # of feet and a number of inches
 # The function should return the equivalent length in meters
 imperial_to_metric <- function(feet, inches) {
-  total.inches <- feet*12 + inches
-  meters <- total.inches * 0.0254
-  meters  # return the value in meters
+  total_inches <- feet * 12 + inches
+  meters <- total_inches * 0.0254
+  meters # return the value in meters
 }
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
-height_in_meters <- imperial_to_metric(5,11)
+height_in_meters <- imperial_to_metric(5, 11)
